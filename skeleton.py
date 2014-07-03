667b0971962f6590d8d224c3a1329a44863a63a6
jefrailey <jcaetof@gmail.com>
2014-07-03 12:49:12 -0700

    Update to buildvocab() to correctly return the N most frequently used words

--- skeleton.py
+++ skeleton.py
@@ -5,19 +5,16 @@ from collections import OrderedDict
 from operator import itemgetter as ig
 from sklearn.linear_model import LogisticRegression as LR
 
-vocab = []  # the features used in the classifier
-
 
 def _build_stopwords():
     stopwords = set(open('stopwords.txt').read().lower().split())
-    bad_chars = set(['\n', "--"])
+    bad_chars = set(['\n', "--", ""])
     stopwords = stopwords.union(bad_chars)
     stopwords = stopwords.union(set([i for i in string.punctuation]))
     return stopwords
 
 
 def buildvocab(N):
-    vocab = []
     vocab1 = {}
     # TODO: Populate vocab list with N most frequent words in training data, minus stopwords
     roots = ['/pos', '/neg']
@@ -37,17 +34,26 @@ def buildvocab(N):
                                 else:
                                     vocab1[word] += 1
 
-    # need to cut vocab list down to N most frequent words
-    vocab = [0 for i in range(N+1)]
-    limiter = 0
-    while len(vocab) > N:
-        vocab = [k for k, v in vocab1.items() if v > limiter]
-        print len(vocab)
-        limiter += 1
+    total_vocab = [(v, k) for k, v in vocab1.items()]
+    total_vocab.sort(reverse=True)
+
     ordered_vocab = OrderedDict()
-    for i, word in enumerate(vocab):
-        ordered_vocab[word] = i
+    for i, word in enumerate(total_vocab):
+        if i == N:
+            break
+        ordered_vocab[word[1]] = i
     return ordered_vocab
+    # need to cut vocab list down to N most frequent words
+    # vocab = [0 for i in range(N+1)]
+    # limiter = 0
+    # while len(vocab) > N:
+    #     vocab = [k for k, v in vocab1.items() if v > limiter]
+    #     print len(vocab)
+    #     limiter += 1
+    # ordered_vocab = OrderedDict()
+    # for i, word in enumerate(vocab):
+    #     ordered_vocab[word] = i
+    # return ordered_vocab
 
 
 def vectorize(review, vocab):
@@ -67,9 +73,9 @@ def make_classifier(vocab):
     y = np.concatenate((pos, neg))
     # TODO: Build X matrix of vector representations of review files, and y vector of labels
     # X = np.zeros((y.shape()[0], len(vocab)))
-    X = np.array([])
     stopwords = _build_stopwords()
     roots = ['/pos', '/neg']
+    temp_list = []
 
     review = []
     for i in roots:
@@ -83,21 +89,22 @@ def make_classifier(vocab):
                         for word in words:
                             if word not in stopwords and word in vocab:
                                 review.append(word)
-                X = np.concatenate((X, vectorize(review, vocab)))
+                temp_list.append(vectorize(review, vocab))
+    X = np.array(temp_list)
+    print X.shape
     lr = LR()
     lr.fit(X, y)
     return lr
 
 
-def test_classifier(lr):
-    global vocab
+def test_classifier(lr, vocab):
     test = np.zeros((len(os.listdir('test')),len(vocab)))
     testfn = []
     i = 0
     y = []
     for fn in os.listdir('test'):
         testfn.append(fn)
-        test[i] = vectorize(os.path.join('test',fn))
+        test[i] = vectorize(os.path.join('test',fn), vocab)
         ind = int(fn.split('_')[0][-1])
         y.append(1 if ind == 3 else -1)
         i += 1
@@ -116,6 +123,6 @@ def test_classifier(lr):
 
 
 if __name__ == '__main__':
-    vocab = buildvocab(100)
+    vocab = buildvocab(10)
     lr = make_classifier(vocab)
-    test_classifier(lr)
+    test_classifier(lr, vocab)
